0b82588ee832777857a94814aa7ea3c9ec17354d
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-01-30 11:52:06 +0800

    Constructors setup

--- Fraction.py
+++ Fraction.py
@@ -2,7 +2,18 @@ class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
         #TODO
-        pass
+        try:
+            self.numerator = numerator
+            self.denominator = denominator
+        except:
+            print("Input not integer")
+
+    def __init__(self, fraction_string: str):
+        #TODO
+        actual_fraction = fraction_string.strip()
+        values = actual_fraction.split(/)
+        self.numerator=int(values[0])
+        self.denominator=int(values[1])
 
     def gcd(a, b):
         #TODO
